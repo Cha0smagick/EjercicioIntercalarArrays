a32e5a70d4105839845633683c219be3cb65eb6e
Cha0smagick <alekos20@gmail.com>
2019-07-08 00:00:59 -0500

    Este es el ejemplo de como mezclar dos arrays en java

--- UsoArrayEjemplo/src/usoarrayejemplo/UsoArrayEjemplo.java
+++ UsoArrayEjemplo/src/usoarrayejemplo/UsoArrayEjemplo.java
@@ -2,7 +2,6 @@
 package usoarrayejemplo;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import javax.swing.JOptionPane;
 
@@ -17,12 +16,12 @@ public class UsoArrayEjemplo {
         List<String> conjunto2 = new ArrayList<>();
         List<String> conjunto3 = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
-        String captura = JOptionPane.showInputDialog("inserte el "+i+" numero");
+        String captura = JOptionPane.showInputDialog("inserte el caracter en la "+i+" posicion");
         conjunto1.add(captura);
            
         }
         for (int i = 0; i < 3; i++) {
-            String captura = JOptionPane.showInputDialog("inserte el "+i+" numero");
+            String captura = JOptionPane.showInputDialog("inserte el caratcer en la "+i+" posicion");
         conjunto2.add(captura);
             
         }
